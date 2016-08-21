22da7fb3e6202436c8422c6eaee97d8cda751538
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-08-21 11:46:16 -0700

    "User input enters a variable correctly"

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 angular.module('app', [])
 
 .controller('MainController', function($scope){
-  $scope.data = {
-    text: "asdfa",
+  $scope.changeLoops = function(code){
+    return code;
+  };
+
+  $scope.execute = function(){
+    const raw = $scope.changeLoops($scope.data.userCode);
+    console.log(raw);
+    //insert let loopChecker = 0; && loopChecker++ <= 1000;
+    
   };
 })
 
-.controller('ChildController', function($scope){
-  
-})
 
 // .config(function($routeProvider){
 //   $routeProvider
